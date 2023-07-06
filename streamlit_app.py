428fce49077c6738c1dc305bfc5e651b2918528d
NimishJ2 <138758576+NimishJ2@users.noreply.github.com>
2023-07-06 13:36:56 +0530

    Update streamlit_app.py
    adding fruit list using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('🥗 Omega 3 and blueberry oat meal')
 streamlit.text('🍹 Spinach and smoothie')
 streamlit.text('🍍 Fruits')
 streamlit.text('🥛 Milk')
 streamlit.header('Create your own smoothie 🍸')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
