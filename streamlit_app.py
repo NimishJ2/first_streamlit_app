5b3636e3a269aa85f6dda1cf9848dd60fccc0f51
NimishJ2 <138758576+NimishJ2@users.noreply.github.com>
2023-07-06 13:24:20 +0530

    Update streamlit_app.py
    added header and few text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menue')
+streamlit.text('South Indian')
+streamlit.text('Fruits')
+streamlit.text('Milk')
