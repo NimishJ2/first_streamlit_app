e4f49cce767da99adb847ecb6b41b7708beadf47
NimishJ2 <138758576+NimishJ2@users.noreply.github.com>
2023-07-06 13:30:20 +0530

    Update streamlit_app.py
    update title
    update text
    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menue')
-streamlit.text('South Indian')
-streamlit.text('Fruits')
-streamlit.text('Milk')
+streamlit.title('My Mom's New Healthy Dinner')
+streamlit.header('🥗 Omega 3 and blueberry oat meal')
+streamlit.text('🍹 Spinach and smoothie')
+streamlit.text('🍍 Fruits')
+streamlit.text('🥛 Milk')
+streamlit.header('Create your own smoothie 🍸')
